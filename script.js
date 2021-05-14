555054cd47cb4af234a03c9193b03e400576377a
matheus-araujop <matheusp.araujo0@gmail.com>
2021-05-14 14:52:48 -0300

    Adicionado Desafio 01

---
+++ script.js
@@ -0,0 +1,12 @@
+let age = 20 // idade
+let year = 2020 // Ano autal
+let birth_year = year-age // Ano de nascimento 
+let phrase = "Meu ano de nascimento é" // frase do console
+
+console.log(phrase+birth_year);
+
+let souhumano = true // variavel booleana
+let x = 3 // numero qualquer
+let y = 8 // numero qualquer
+
+console.log(x+y);
